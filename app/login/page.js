0621d3d3467ac493b9171bb272f0d14a8e4df954
pfyffer86 <pfyffer86@gmail.com>
2026-03-30 20:50:03 +0700

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { useState } from "react"
+import { supabase } from "@/lib/supabase"
+import { useRouter } from "next/navigation"
+
+export default function LoginPage() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter()
+
+  async function handleLogin() {
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    localStorage.setItem("token", data.session.access_token)
+
+    router.push("/dashboard")
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login</h1>
+
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
