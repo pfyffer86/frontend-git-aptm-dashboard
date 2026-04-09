43444e099aa7bed15b8c0eed0f7478ce28f0bb79
pfyffer86 <pfyffer86@gmail.com>
2026-04-10 00:48:17 +0700

    Update Header.js
    header polish

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,16 +1,29 @@
 "use client"
 
+import { IconRefresh, IconUser } from "@tabler/icons-react"
+
 export default function Header() {
 
   return (
     <div className="header">
 
-      <button className="button-primary">
-        Refresh
-      </button>
+      {/* LEFT */}
+      <div className="header-left">
+        <h2 className="header-title">Assets</h2>
+      </div>
 
-      <div className="user-button" style={{ marginLeft: 12 }}>
-        U
+      {/* RIGHT */}
+      <div className="header-right">
+
+        <button className="button-primary header-btn">
+          <IconRefresh size={16} />
+          Refresh
+        </button>
+
+        <div className="user-button">
+          <IconUser size={16} />
+        </div>
+
       </div>
 
     </div>
