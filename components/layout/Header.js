28a80f1f3145ea2dffdd98772a5ed52a3e5db9c0
pfyffer86 <pfyffer86@gmail.com>
2026-04-10 11:16:23 +0700

    Update Header.js

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -21,10 +21,6 @@ export default function Header() {
      <div className="header">
      <div />
 
-  <div className="header-right">
-    ...
-  </div>
-
 </div>
 
       <div className="header-right">
